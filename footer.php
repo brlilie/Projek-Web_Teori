7ad88845efd471a6658c1c3d975567e2c1953540
Ratu Berliana Rosfadila Machendra <151343648+brlilie@users.noreply.github.com>
2025-06-04 18:28:33 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="text-center py-4 mt-5 text white">
+  <div class="container">
+    <p class="mb-0">&copy; <?= date("Y") ?> Projek Web, All right reserved.</p>
+  </div>
+</footer>
---
+++ navbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-light shadow-sm">
+  <div class="container">
+    <a class="navbar-brand fw-bold fs-6" href="index.php">Lombain</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="ms-2 my-2 "><a class="nav-link text-white border border-white rounded-pill d-inline-block px-4 py-2" href="home.php">Home</a> </li>
+        <li class="ms-2 my-2"><a class="nav-link text-white border border-white rounded-pill d-inline-block px-4 py-2" href="booklet.php">Booklet</a></li>
+      </ul>
+
+    </div>
+  </div>
+</nav>
+
