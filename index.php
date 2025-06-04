8113145fc0235659c383ed5d4f684f77a4f41dfe
Ratu Berliana Rosfadila Machendra <151343648+brlilie@users.noreply.github.com>
2025-06-04 18:27:38 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Tech Competition</title>
+  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+ 
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/navbar.css">
+  <link href="https://fonts.googleapis.com/css2?family=Krona+One&display=swap" rel="stylesheet">
+</head>
+<body>
+<?php require_once('layout/navbar_landing.php'); ?>
+
+  <section class="text-white text-center hero-section">
+  <div class="container">
+    <h1 class="display-4">Lombain 1.0</h1>
+    <p class="lead">Cari Lomba di Bidang IT? Lombain aja!</p>
+    <div class="mt-4 section-buttons">
+      <a class="nav-link text-white border border-white rounded-pill d-inline-block px-4 py-2" href="login.php">Login</a>
+      <a class="nav-link text-white border border-white rounded-pill d-inline-block px-4 py-2" href="register.php">Daftar</a>
+    </div>
+  </div>
+</section>
+
+
+
+  <?php require_once('layout/footer.php'); ?>
+
+ 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
