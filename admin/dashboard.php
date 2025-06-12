7c3d79f1f0c157f65bcb398031fe627dc547a76e
Ratu Berliana Rosfadila Machendra <151343648+brlilie@users.noreply.github.com>
2025-06-12 16:47:08 +0700

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -6,7 +6,7 @@
     <title>Dashboard Admin - LombaApps</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+VUe2MyuLh1+ALv4G9iC4b4B09pYf5b+Q0/c8BqFw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="../assets/css/style.css"> <style>
+   <style>
         body {
             background-color: #f8f9fa; /* Warna latar belakang umum */
         }
