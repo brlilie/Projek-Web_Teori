805d66683cb59d9f77b515ea65d4ae5d31dcedc7
Ratu Berliana Rosfadila Machendra <151343648+brlilie@users.noreply.github.com>
2025-06-18 21:22:10 +0700

    Update update_lomba_proses.php

--- proses/update_lomba_proses.php
+++ proses/update_lomba_proses.php
@@ -19,7 +19,7 @@ if ($_POST['form_mode'] === 'edit') {
     if (!empty($_FILES['poster_lomba']['name'])) {
         $poster_name = basename($_FILES['poster_lomba']['name']);
         $poster_tmp = $_FILES['poster_lomba']['tmp_name'];
-        $poster_path = 'uploads/poster/' . $poster_name;
+        $poster_path = 'assets/images/' . $poster_name;
 
         move_uploaded_file($poster_tmp, '../' . $poster_path);
     }
