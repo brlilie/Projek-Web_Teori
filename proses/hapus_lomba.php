88ae27f32fee72a555ce39fcb92165b299bc4fe6
Ratu Berliana Rosfadila Machendra <151343648+brlilie@users.noreply.github.com>
2025-06-18 21:31:51 +0700

    Update hapus_lomba.php

--- proses/hapus_lomba.php
+++ proses/hapus_lomba.php
@@ -4,12 +4,12 @@ require_once '../config/db.php';
 if (isset($_GET['id'])) {
     $id = intval($_GET['id']);
 
-    // Optional: ambil poster lama untuk dihapus juga dari server
+    
     $poster_query = $conn->query("SELECT poster_lomba FROM daftar_lomba WHERE id_lomba = $id");
     $poster_data = $poster_query->fetch_assoc();
     $poster_path = '../' . $poster_data['poster_lomba'];
 
-    // Hapus dari DB
+   
     $query = "DELETE FROM daftar_lomba WHERE id_lomba = ?";
     $stmt = $conn->prepare($query);
     $stmt->bind_param("i", $id);
